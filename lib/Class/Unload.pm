42766b903412c34e5478d841303354ce6159af66
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2010-04-26 20:34:24 +0100

    Convert to Dist::Zilla

--- lib/Class/Unload.pm
+++ lib/Class/Unload.pm
@@ -1,4 +1,5 @@
 package Class::Unload;
+# ABSTRACT: Unload a class
 
 use warnings;
 use strict;
@@ -6,26 +7,7 @@ no strict 'refs'; # we're fiddling with the symbol table
 
 use Class::Inspector;
 
-=head1 NAME
-
-Class::Unload - Unload a class
-
-=head1 VERSION
-
-Version 0.05
-
-=cut
-
-use 5.005;
-use vars qw($VERSION);
-
-BEGIN {
-	$VERSION = '0.05';
-}
-
 =head1 SYNOPSIS
-
-Unload a class
 
     use Class::Unload;
     use Class::Inspector;
@@ -37,9 +19,7 @@ Unload a class
 
     require Some::Class; # Reloads the class
 
-=head1 METHODDS
-
-=head2 unload $class
+=method unload $class
 
 Unloads the given class by clearing out its symbol table and removing it
 from %INC.
@@ -67,72 +47,14 @@ sub unload {
     return 1;
 }
 
-=head1 AUTHOR
-
-Dagfinn Ilmari Mannsåker, C<< <ilmari at ilmari.org> >>
-
 =head1 SEE ALSO
 
 L<Class::Inspector>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-class-unload at
-rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Class-Unload>. I will
-be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Class::Unload
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Class-Unload>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Class-Unload>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Class-Unload>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Class-Unload>
-
-=item * Git reposiory
-
-L<http://git.ilmari.org/?p=Class-Unload.git>
-
-C<git://git.ilmari.org/Class-Unload.git>
-
-
-=back
-
 
 =head1 ACKNOWLEDGEMENTS
 
 Thanks to Matt S. Trout, James Mastros and Uri Guttman for various tips
 and pointers.
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Dagfinn Ilmari Mannsåker.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 
 =cut
 
